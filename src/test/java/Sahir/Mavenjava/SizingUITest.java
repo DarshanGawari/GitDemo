1322e1f8cf277492900bed30eb3df3a2e3932dc9
DarshanGawari <darshan.gawari0@gmail.com>
2021-06-07 10:41:03 +0530

    First commit

---
+++ src/test/java/Sahir/Mavenjava/SizingUITest.java
@@ -0,0 +1,18 @@
+package Sahir.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class SizingUITest {
+	
+	@Test
+	public void SizingUI() 
+	{
+		System.out.println("Szing UI");
+	}
+	@Test
+	public void Sizingunit() 
+	{
+		System.out.println("Szing UI");
+	}
+
+}
---
+++ src/test/java/Sahir/Mavenjava/databaseTest.java
@@ -0,0 +1,20 @@
+package Sahir.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class databaseTest {
+
+	@Test
+	public void Database() 
+	{
+		System.out.println("Database ");
+	}
+	
+	@Test
+	public void DataString() 
+	{
+		System.out.println("Datastring");
+	}
+	
+	
+}
